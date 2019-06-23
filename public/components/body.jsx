903f477352aff6ec0ae302e8106a9ffe4e8ba5db
hashiniw <hash_hw@icloud.com>
2019-06-23 09:15:18 +0530

    fourth commit from Hashini

--- public/components/body.jsx
+++ public/components/body.jsx
@@ -3,6 +3,7 @@ import Home from "./home";
 import SignIn from "./sign_in";
 import Courses from "./courses";
 import Signup from "./signup";
+import Admin from "./admin";
 import AdminCreate from "./createAdmin";
 import Instructor from "./instructor";
 import AssignInstructor from "./assignInstructor";
@@ -17,6 +18,7 @@ class Body extends Component {
                     <Route path="/sign_in" component={SignIn} ></Route>
                     <Route path="/sign_up" component={Signup} ></Route>
                     <Route path="/courses" component={Courses} ></Route>
+                    <Route path="/admin" component={Admin} ></Route>
                     <Route path="/adminCreate" component={AdminCreate} ></Route>
                     <Route path="/instructor" component={Instructor} ></Route>
                     <Route path="/assignInstructor" component={AssignInstructor} ></Route>
