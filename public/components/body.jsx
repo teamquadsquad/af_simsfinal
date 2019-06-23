e6dc7bf3c1fc35c8917e349e35517c3e2def0289
vikumR <vikumravishanka@gmail.com>
2019-06-23 13:19:11 +0530

    Revert "2nd Commit - Assignments - Instructor"
    This reverts commit d4c862955d2aeffc2830611f6ec19b8ac1666bde.

--- public/components/body.jsx
+++ public/components/body.jsx
@@ -3,7 +3,6 @@ import Home from "./home";
 import SignIn from "./sign_in";
 import Courses from "./courses";
 import Signup from "./signup";
-import Assignment from "./assignmentsComponent";
 import { Route, Link, Switch } from 'react-router-dom';
 
 class Body extends Component {
@@ -14,7 +13,6 @@ class Body extends Component {
                     <Route path="/sign_in" component={SignIn} ></Route>
                     <Route path="/sign_up" component={Signup} ></Route>
                     <Route path="/courses" component={Courses} ></Route>
-                    <Route path="/assignments" component={Assignment} ></Route>
                     <Route exact path="/" component={Home} ></Route>
                 </Switch>
             </div>
--- server.js
+++ server.js
@@ -15,45 +15,17 @@ mongoose.connect('mongodb://localhost:27017/sims').then(() => {
     process.exit(-1);
 });
 
+app.use('/api/rooms', require('./app/routes/room.server.routes'));
+app.use('/api/categories', require('./app/routes/category.server.routes'));
 app.use('/api', require('./app/routes/api'));
+
 
 app.use(bundler.middleware());
 
 app.use(express.static('./dist'));
-app.use('uploadsInstructor', express.static('uploadsInstructor'));
 
 app.get('/', function (req, res) {
     res.sendFile('./dist/index.html');
-});
-
-//To prevent CORS errors
-//append headers to any response sent
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-});
-
-//Error handling
-app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
-});
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
 });
 
 app.listen(3000, err => {
@@ -61,6 +33,7 @@ app.listen(3000, err => {
         console.error(err);
         return;
     }
+
     console.log('Application is running on port 3000');
 });
 
